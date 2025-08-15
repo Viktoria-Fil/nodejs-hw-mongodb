37783d63067fe359ac7ea18f68210f429a8b341d
Viktoria Fil <sundywave.u@gmail.com>
2025-08-15 23:53:07 +0300

    add-ignore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import setupServer from './server.js';
+import fs from 'fs';
+import path from 'path';
+
 
 const bootstrap = async () => {
   try {
     await initMongoConnection();
+
+    const tmpPath = path.resolve('src', 'tmp');
+    if (!fs.existsSync(tmpPath)) {
+      fs.mkdirSync(tmpPath, { recursive: true });
+      console.log('Created tmp folder');
+    }
+
     setupServer();
   } catch (error) {
     console.log(error);
   }
 };
+
 bootstrap();
